13b6410838da9be322e7366d252c5563d07dc1af
SebastiaoKomada <sebastiao.komada15@hotmail.com>
2025-08-07 19:19:15 -0300

    Configuracao inicial - Java Spring Boot v3.4.0

--- src/modules/evidence/dto.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-use uuid::Uuid;
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CreateEvidenceDto {
-    pub camera_id: Uuid,
-    pub file_path: String,
-    pub description: String,
-}
--- src/modules/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod users;
-pub mod keycloak;
-pub mod occurrences;
-pub mod evidence;
-pub mod settings;
